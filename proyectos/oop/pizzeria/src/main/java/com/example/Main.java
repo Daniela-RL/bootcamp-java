e61d6ee1e2bb3bc884c426bed9249a80ec18d25c
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2024-05-24 19:11:35 +0200

    cambiar el nombre de una variable y agregar una funcion para darle formato a la fecha y escribirla por pantalla.

--- proyectos/oop/pizzeria/src/main/java/com/example/Main.java
+++ proyectos/oop/pizzeria/src/main/java/com/example/Main.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -11,8 +12,8 @@ public class Main {
         Ingrediente ingrediente2 = new Ingrediente(2, "Tomate", 5, 10);
         Ingrediente ingrediente3 = new Ingrediente(3, "Jamón", 5, 10);
         
-        List<Ingrediente> ingredientsList = List.of(ingrediente, ingrediente2, ingrediente3);
-        return ingredientsList;
+        List<Ingrediente> ingredients = List.of(ingrediente, ingrediente2, ingrediente3);
+        return ingredients;
     }
 
     private static Pizza getPizza1(){
@@ -28,13 +29,13 @@ public class Main {
     }
 
     private static Pedido getPedido1(){
-        Pedido pedido = new Pedido(1, new Date(2024, 5, 21));
+        Pedido pedido = new Pedido(1, new Date());
         pedido.addPizza(getPizza1());
         pedido.addPizza(getPizza2());
         return pedido;
     }
     private static Pedido getPedido2(){
-        Pedido pedido = new Pedido(1, new Date(2024, 5, 21));
+        Pedido pedido = new Pedido(1, new Date());
         pedido.addPizza(getPizza2());
         pedido.addPizza(getPizza2());
         return pedido;
@@ -45,23 +46,30 @@ public class Main {
             p.ingredientes.get(i).unitCount++;
         }
     }
+    private static String getDateFormat(){
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String requiredDate = dt.format(new Date());
+        return requiredDate;
+
+    }
 
     public static void main(String[] args) {
-        List<Ingrediente> ingredientsList = getIngredientes();
-        Pizzeria pizzeria = new Pizzeria(ingredientsList);
+        List<Ingrediente> ingredientes = getIngredientes();
+        Pizzeria pizzeria = new Pizzeria(ingredientes);
         Pedido pedido = getPedido1();
         Pedido pedido2 = getPedido2();
 
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Date of order: " + getDateFormat());
         System.out.println("Options : ");
         System.out.println("    0. Exit");
         System.out.println("    1. See ingredients");
         System.out.println("    2. Create order 1");
-        System.out.println("        Pizza 1: " + getPizza1().getUsedIngredientsStr(ingredientsList));
-        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientsList));
+        System.out.println("        Pizza 1: " + getPizza1().getUsedIngredientsStr(ingredientes));
+        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientes));
         System.out.println("    3. Create order 2");
-        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientsList));
-        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientsList));
+        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientes));
+        System.out.println("        Pizza 2: " + getPizza2().getUsedIngredientsStr(ingredientes));
         System.out.println("    4. Add ingredients");
         System.out.println("        Jamón: 1");
         System.out.println("        Tomate: 1");
